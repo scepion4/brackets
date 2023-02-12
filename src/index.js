f8352cf97aadd9ffced34a264a5b657064afea99
scepion4 <safonov04@ukr.net>
2023-02-12 13:53:29 +0200

    feat:resolve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   let stack=[]
+   let brackets={}
+   let check=[]
+   bracketsConfig.forEach(e=>{
+     brackets[e[1]]=e[0]
+     check.push(e[1])
+   })
+   for(let i=0;i<str.length;i++){
+    
+     if(str[i]==brackets[str[i]]&&check.includes(str[i])&&stack.includes(str[i])||str[i]!==brackets[str[i]]&&check.includes(str[i])){
+    if(brackets[str[i]]!==stack.pop()) return false 
+   }else{
+   stack.push(str[i])
+   }
+   }
+   return stack.length==0
 }
+
+
